30853e3733e86faf039d8a4f0bb41d324409fd37
MichaelPerkins2 <michael.perkins.d@gmail.com>
2018-10-25 14:41:17 -0600

    added delete function

--- src/App.js
+++ src/App.js
@@ -12,4 +12,16 @@ class App extends Component {
   }
 }
 
+
+let btns = document.querySelectorAll('#book-list .delete');
+
+Array.from(btns).forEach(function(btn){
+    btn.addEventListener('click',function(e){
+
+        const li = e.target.parentElement;
+        li.parentNode.removeChild(li);
+
+    });
+});
+
 export default App;
